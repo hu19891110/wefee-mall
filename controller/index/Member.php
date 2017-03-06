d70a6884a30e6139239cf82d465eef8e533f1124
轻色年华 <13675626825@163.com>
2017-03-06 16:26:05 +0800

    完成账号安全

--- controller/index/Member.php
+++ controller/index/Member.php
@@ -2,12 +2,16 @@
 
 use addons\wefeemall\lib\AuthManage;
 use addons\wefeemall\model\MallUserAddress;
+use addons\wefeemall\model\MallUsers;
 use addons\wefeemall\traits\LoginCheck;
+use addons\wefeemall\traits\MobileVerifyCodeCheck;
+use addons\wefeemall\traits\VerifyCodeCheck;
+use think\helper\Hash;
 use think\Validate;
 
 class Member extends Base
 {
-    use LoginCheck;
+    use LoginCheck,VerifyCodeCheck,MobileVerifyCodeCheck;
 
     public function _initialize()
     {
@@ -91,6 +95,106 @@ class Member extends Base
         MallUserAddress::get(request()->param('id'))->delete();
 
         $this->success('操作成功', aurl('wefeemall/index.member/myAddress'));
+    }
+
+    public function safe()
+    {
+        $user = AuthManage::user();
+
+        $title = '账号安全';
+
+        return view(VIEW_PATH . '/index/member/safe/index.html', compact('title', 'user'));
+    }
+
+    public function changePass()
+    {
+        $title = '修改密码';
+
+        return view(VIEW_PATH . '/index/member/safe/change_pass.html', compact('title'));
+    }
+
+    public function postChangePass()
+    {
+        $this->checkVerifyCode();
+
+        $data = $this->changePassValidator();
+
+        $user = AuthManage::user();
+
+        if (! Hash::check($data['old_password'], $user->password)) {
+            $this->error('原密码不正确', aurl('wefeemall/index.member/changePass'));
+        }
+
+        $user->password = Hash::make($data['new_password']);
+        $user->save();
+
+        $this->success('密码修改成功');
+    }
+
+    protected function changePassValidator()
+    {
+        $data = request()->only([
+            'old_password', 'new_password', 'new_password_confirm',
+        ]);
+
+        $validator = new Validate([
+            'new_password|新密码' => 'require|min:6|max:16|confirm',
+        ]);
+
+        if (! $validator->check($data)) {
+            $this->error($validator->getError());
+        }
+
+        return $data;
+    }
+
+    public function changeMobile()
+    {
+        $user = AuthManage::user();
+
+        $title = '更换手机号';
+
+        return view(VIEW_PATH . '/index/member/safe/change_mobile.html', compact('title', 'user'));
+    }
+
+    public function postChangeMobile()
+    {
+        $this->checkVerifyCode();
+
+        $this->mobileVerifyCodeCheck();
+
+        $data = $this->changeMobileValidator();
+
+        $user = AuthManage::user();
+
+        if (! Hash::check($data['password'], $user->password)) {
+            $this->error('密码不正确', aurl('wefeemall/index.member/changeMobile'));
+        }
+
+        $user->mobile = $data['username'];
+        $user->save();
+
+        $this->success('手机号更换成功');
+    }
+
+    protected function changeMobileValidator()
+    {
+        $data = request()->only([
+            'username', 'password',
+        ]);
+
+        $validator = new Validate([
+            'username|手机号' => 'require|mobile|unique:mall_users',
+        ]);
+        $validator->extend('mobile', function ($value) {
+            return preg_match('/^(((13[0-9]{1})|(15[0-35-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/', $value) === 1 ? true : false;
+        });
+
+        if (! $validator->check($data)) {
+            $this->error($validator->getError());
+        }
+
+        return $data;
     }
 
 }
